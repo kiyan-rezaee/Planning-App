e4b8982796724e79c8ace956e7948d1e631d30d4
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2021-10-11 02:24:33 +0330

    updated

--- PlanningWebApp/settings.py
+++ PlanningWebApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bi$l!q&jn+=$!63-byce293+!i%!3sebc^wj0sthb_9or3sd#=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'studier-app.herokuapp.com']
 
 # Application definition
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
