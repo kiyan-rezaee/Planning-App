6fbc2778d09f6c1a4aa5bbee7f13adff1a715953
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2021-10-18 19:13:28 +0330

    updated

--- PlanningWebApp/settings.py
+++ PlanningWebApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 import django_heroku
 
@@ -73,10 +74,7 @@ WSGI_APPLICATION = 'PlanningWebApp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
