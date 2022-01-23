e6dca982b504c5e1618dd08a609de1081061a10f
vahidaghili <vahidaghilizadeh.com@gmail.com>
2022-01-23 14:32:49 +0330

    .

--- PlanningWebApp/settings.py
+++ PlanningWebApp/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 
 import django_heroku
 
+SESSION_ENGINE= 'django.contrib.sessions.backends.cached_db'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
